a6898c7690704354428f84f19484b9a6167bf2e9
Arash Khan <akhan@verisys.com>
2016-07-21 23:04:32 -0600

    tests done

--- contact book/app/controllers/contacts_controller.rb
+++ contact book/app/controllers/contacts_controller.rb
@@ -11,7 +11,10 @@ class ContactsController < ApplicationController
 
 
   def create
-      Contact.create(strong_params_helper)
+      if check_hash_values
+        Contact.create(strong_params_helper)
+      end
+
       redirect_to contacts_path
   end
 
@@ -23,7 +26,9 @@ class ContactsController < ApplicationController
 
   def update
       contact = Contact.find(params[:id])
-      contact.update(strong_params_helper true)
+      if check_hash_values
+        contact.update(strong_params_helper true)
+      end
       redirect_to contacts_path
   end
 
@@ -31,17 +36,25 @@ class ContactsController < ApplicationController
   private
 
 
-  def strong_params_helper(is_active=nil)
+  # Returns 'true' if 'params' has some values in it's hash
+  def check_hash_values
+    hash = params[:contact].deep_dup
+    hash.delete_if {|k, v| v == ""}
+    !hash.empty?
+  end
+
+
+  def strong_params_helper(pass_active=nil)
 
       parameters = [:dept_name, :contact_name, :business_phone,
                     :business_extension, :home_phone, :emergency_contact,
                     :emergency_phone]
-      if is_active
+      if pass_active
           parameters << :active
           params.require(:contact).permit(*parameters)
 
       else
-          params.require(:contact).permit(parameters)
+          params.require(:contact).permit(*parameters)
       end
 
   end
