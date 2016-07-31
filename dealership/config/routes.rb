72c0b5c87bbe90b7d6ac00a56d8f4eb530a673d6
OsagieDev <OsagieO@gmail.com>
2016-07-31 17:32:01 -0500

    Removed unused files

--- dealership/config/routes.rb
+++ dealership/config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
 
   get '/welcome', to: 'welcome#index'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
