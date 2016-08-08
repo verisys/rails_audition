148a1b3115365e7a273f41887c8f1c3b59612236
aard <adam.ard2011@gmail.comm>
2016-08-08 12:35:54 -0600

    bootstrapped

---
+++ 001_contacts/setup.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+sudo apt-get update
+#sudo apt-get -y install ruby-rails-4.0
+#sudo apt-get -y install bundler
+
+
+sudo apt-get -y install docker.io
+
+
+
+# bootstrap
+# cd /vagrant
+# sudo docker build -t rails .
+# sudo docker run -v /vagrant:/usr/src/app rails rails new contact_book --api
+
+# instructions
+# cd /vagrant
+# sudo docker build -t rails .
