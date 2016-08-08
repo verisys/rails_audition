67383c627ceed178d94ee660b123f3de91407cab
aard <adam.ard2011@gmail.comm>
2016-08-08 14:55:38 -0600

    additions to make basic rails app work

--- 001_contacts/setup.sh
+++ 001_contacts/setup.sh
@@ -1,19 +1,20 @@
 #/bin/bash
 
 sudo apt-get update
-#sudo apt-get -y install ruby-rails-4.0
-#sudo apt-get -y install bundler
-
-
 sudo apt-get -y install docker.io
-
-
 
 # bootstrap
 # cd /vagrant
 # sudo docker build -t rails .
 # sudo docker run -v /vagrant:/usr/src/app rails rails new contact_book --api
 
-# instructions
+# make docker container
 # cd /vagrant
 # sudo docker build -t rails .
+# ...
+
+# development
+# cd /vagrant
+# sudo docker run -it -p 3000:3000 -v /vagrant:/usr/src/app rails /bin/bash
+# $ bundle install 
+# $ rails s
