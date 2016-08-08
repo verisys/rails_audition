a41d5629c2896a95f942ecd824156564942d5185
aard <adam.ard2011@gmail.comm>
2016-08-08 15:15:40 -0600

    fixed memory problem with the vagrant vm

--- 001_contacts/setup.sh
+++ 001_contacts/setup.sh
@@ -8,13 +8,19 @@ sudo apt-get -y install docker.io
 # sudo docker build -t rails .
 # sudo docker run -v /vagrant:/usr/src/app rails rails new contact_book --api
 
-# make docker container
+# run
+# vagrant ssh
 # cd /vagrant
 # sudo docker build -t rails .
-# ...
+# sudo docker run -it -p 3000:3000 -v /vagrant:/usr/src/app rails
+#  ? add bundle install into the Dockerfile
+# go to localhost:3000 in your browser
+
 
 # development
+# vagrant ssh
 # cd /vagrant
+# sudo docker build -t rails .
 # sudo docker run -it -p 3000:3000 -v /vagrant:/usr/src/app rails /bin/bash
 # $ bundle install 
 # $ rails s
