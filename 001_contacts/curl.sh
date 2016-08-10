6dd8edfeed756d9f23b808633fcd6babaea8c225
aard <adam.ard2011@gmail.comm>
2016-08-10 12:48:11 -0600

    added stubs for the web pages

--- 001_contacts/curl.sh
+++ 001_contacts/curl.sh
@@ -11,8 +11,8 @@ curl -i -XGET 'http://localhost:3000/api/v1/contacts/1'
 
 # Add contact
 curl -i -H 'Content-Type: application/json' -XPOST 'http://localhost:3000/api/v1/contacts' -d '{
-       "contact": {"name":"Adam Ard",
-                     "dept":"Engineering",
+       "contact": {"name":"George Washington",
+                     "dept":"Sales",
                      "business_phone":"555-555-5555",
                      "business_phone_ext":"555",
                      "home_phone":"555-555-5554",
