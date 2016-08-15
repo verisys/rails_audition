02d81103afe948209bbae617c4d0edc826768688
Joe Hawkins <jedwardhawkins@gmail.com>
2016-08-14 18:06:46 -0600

    Added unique constraint to supervisor_id column on departments table

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,6 @@
 class Department < ActiveRecord::Base
   validates_presence_of :name
+  validates_uniqueness_of :supervisor_id
 
   belongs_to :supervisor, class_name: 'User'
 end
--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,3 +1,4 @@
+require 'factory_girl'
 require 'rails_helper'
 require 'support/validation_examples'
 
@@ -5,4 +6,11 @@ describe Department, :type => :model do
   subject { described_class.new }
 
   include_examples 'required attributes example', [:name]
+
+  it 'validates uniqueness of supervisor' do
+    user = FactoryGirl.create(:user)
+    department_1 = FactoryGirl.create(:department, supervisor: user)
+    department_2 = FactoryGirl.build(:department, supervisor: user)
+    expect(department_2.valid?).to be_falsey
+  end
 end
