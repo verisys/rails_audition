63adf38883e331072c7024f614f8127d9ae7aef5
erikb <erikburton1@gmail.com>
2016-08-24 22:54:50 -0600

    changes int to integer

--- contactsapp/db/migrate/20160825045202_create_contacts.rb
+++ contactsapp/db/migrate/20160825045202_create_contacts.rb
@@ -4,7 +4,7 @@ class CreateContacts < ActiveRecord::Migration[5.0]
       t.string :dept_name, null:false
       t.string :contact_name, null:false
       t.bigint :business_phone, null:false
-      t.int :extension
+      t.integer :extension
       t.bigint :home_phone, null:false
       t.string :emergency_contact_name, null:false
       t.bigint :emergency_contact_phone, null:false
