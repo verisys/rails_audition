657c0baae345d2f69d2f920611ed33baf5bd2484
erikb <erikburton1@gmail.com>
2016-08-24 23:38:01 -0600

    Changes root page to contacts#index

--- contactsapp/config/routes.rb
+++ contactsapp/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :contacts
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'contacts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
