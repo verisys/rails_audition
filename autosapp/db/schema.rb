810724a8f2448859830b925f298ac8fd66271237
erikb <erikburton1@gmail.com>
2016-08-25 21:11:54 -0600

    Adds jumbotron to inventories index and adds class table to inventory list.

--- autosapp/db/schema.rb
+++ autosapp/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826020111) do
+ActiveRecord::Schema.define(version: 20160826025756) do
+
+  create_table "inventories", force: :cascade do |t|
+    t.integer  "year"
+    t.string   "make"
+    t.string   "model"
+    t.decimal  "price"
+    t.string   "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string   "location_name"
