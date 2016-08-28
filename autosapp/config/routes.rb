281da83b9864cf7a1e663624f1a865ac319de235
Briant Gale <briantgale@gmail.com>
2016-08-28 17:33:44 -0600

    Added locations and policy for locations

--- autosapp/config/routes.rb
+++ autosapp/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'user/registrations' }
+
   resources :homes, only: [:index]
+  resources :locations, execpt: [:show, :destroy]
 
   root to: 'homes#index'
 end
