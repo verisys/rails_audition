9660c8506adc3e30386970dc78ff926b1faa5fa3
Briant Gale <briantgale@gmail.com>
2016-08-28 20:14:35 -0600

    Testing is working!  Added validation that should have been added to location

--- autosapp/app/models/location.rb
+++ autosapp/app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ApplicationRecord
+  has_many :vehicles
+
+  validates_presence_of :name
 end
--- autosapp/spec/models/location_spec.rb
+++ autosapp/spec/models/location_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  context "is valid with a name" do
-    it "is valid with a name" do
-      location = Location.create(name: "My Happy Location")
+  it "is valid with a name" do
+    location = Location.create(name: "My Happy Location")
 
-      expect(location).to be_valid
-    end
+    expect(location).to be_valid
+  end
 
-    it "is invalid without a name" do
-      location = Location.create(name: nil)
+  it "is invalid without a name" do
+    Location.create(name: nil)
 
-      location.should have(1).errors_on(:name)
-    end
+    expect(Location.count).to eq(0)
   end
 end
