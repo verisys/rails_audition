2336280716ddae473e329b44d5e0e82d74e9702a
Allen Maxwell <aamax@xmission.com>
2016-12-08 07:15:10 -0700

    Initial app setup, pre auth, models and controllers

--- autosapp/config/routes.rb
+++ autosapp/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/:id' => 'pages#show'
+
+  root :to => "pages#show"
 end
