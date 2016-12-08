dda06a06f0587433e19827b00341c13c99281436
Veronica Cannon <me@veronicacannon.com>
2016-12-08 08:16:23 -0800

    Establish routes.

--- contactbook/config/routes.rb
+++ contactbook/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root 'supervisors#login'
+  resources :departments, only: [:index], :path => '/' do
+    resources :contacts, except: [:show] do
+    	 get "toggle_inactive", :on => :member
+    end
+  end
 end
